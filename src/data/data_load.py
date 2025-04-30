18424f4df0a1d55a9010ca0527551b1c4c74ec12
Ahmed <117286352+AhmedProj@users.noreply.github.com>
2025-05-01 00:05:53 +0200

    Data load with s3

--- src/data/data_load.py
+++ src/data/data_load.py
@@ -1,25 +1,39 @@
-import os
-import s3fs
+import boto3
 import pandas as pd
+import pyarrow as pa
 import pyarrow.parquet as pq
+from io import BytesIO
+from botocore.exceptions import ClientError
 
-# Create filesystem object
-S3_ENDPOINT_URL = "https://" + os.environ["AWS_S3_ENDPOINT"]
-BUCKET = "ahmed" # "danalejo" #
-FILE_PATH_S3 = BUCKET + "/diffusion/"
+BUCKET = "bucketest-1214"
+PREFIX = "diffusion/"
+CSV_FILE = "training.csv"
 
-
-fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": S3_ENDPOINT_URL}, key = os.environ["AWS_ACCESS_KEY_ID"], secret = os.environ["AWS_SECRET_ACCESS_KEY"])
-
-def load_csv(file="training.csv", sep=";"):
-    with fs.open(FILE_PATH_S3 + file, mode="rb") as file:
-        df = pd.read_csv(file, sep=sep)
-    return df
+s3_client = boto3.client("s3")
 
 
-def send_csv(df, name):
-    with fs.open(FILE_PATH_S3 + name, "w") as file:
-        df.to_csv(file)
+def load_csv(file=CSV_FILE):
+    """Loads the CSV file from S3"""
+    print(f"📥 Loading CSV from S3: {file}")
+    csv_obj = s3_client.get_object(Bucket=BUCKET, Key=PREFIX + file)
+    return pd.read_csv(csv_obj['Body'], sep=";")
 
-def send_parquet(table, name):
-    pq.write_to_dataset(table, FILE_PATH_S3 + name, filesystem=fs)
+
+def send_csv(df, file_name):
+    """Uploads the given DataFrame as a CSV to S3"""
+    key = PREFIX + file_name
+    buffer = BytesIO()
+    df.to_csv(buffer, index=False)
+    buffer.seek(0)
+    s3_client.put_object(Bucket=BUCKET, Key=key, Body=buffer.getvalue())
+    print(f"📤 CSV uploaded to S3: {file_name}")
+
+def send_parquet(df, file_name):
+    """Converts the DataFrame to Parquet and uploads it to S3"""
+    key = PREFIX + file_name
+    table = pa.Table.from_pandas(df)
+    buffer = BytesIO()
+    pq.write_table(table, buffer)
+    buffer.seek(0)
+    s3_client.put_object(Bucket=BUCKET, Key=key, Body=buffer.getvalue())
+    print(f"📤 Parquet uploaded to S3: {file_name}")
